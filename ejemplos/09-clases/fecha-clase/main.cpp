dab5df591680ee1beb6e95333e491ab8b7b32be4
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2021-05-30 19:43:46 +0200

    Fecha con clases

--- ejemplos/09-clases/fecha-clase/main.cpp
+++ ejemplos/09-clases/fecha-clase/main.cpp
@@ -1,12 +1,10 @@
+#include "fecha.hpp"
 #include <iostream>
-using namespace std;
-
-#include "fecha.h"
 
 int main() {
   fecha f;
   f.inicia(4, 9, 1969);
   f.suma_dia(60);
-  cout << "Fecha: ";
+  std::cout << "Fecha: ";
   f.imprime();
 }
