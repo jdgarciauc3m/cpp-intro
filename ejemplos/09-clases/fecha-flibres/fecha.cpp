1b1f1554440088c1d20cd23fa0c1728c6e8ccd20
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2021-05-30 11:18:28 +0200

    Fecha con funciones libres

--- ejemplos/09-clases/fecha-flibres/fecha.cpp
+++ ejemplos/09-clases/fecha-flibres/fecha.cpp
@@ -1,5 +1,6 @@
 #include "fecha.hpp"
 #include <mincontracts/mincontracts.hpp>
+#include <vector>
 
 void inicia(fecha & f, int d, int m, int a) {
   f.dia = d;
@@ -17,23 +18,24 @@ void suma_mes(fecha & f, int m) {
   f.anyo += (nm - 1) / 12;
 }
 
-bool es_bisiesto(int a) {
-  return ((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0);
-}
-
-int dias_mes(int m, int a) {
-  switch (m) {
-    case 1: case 3: case 5: case 7:
-    case 8: case 10: case 12:
-      return 31;
-    case 4: case 6: case 9: case 11:
-      return 30;
-    case 2:
-      return es_bisiesto(a) ? 29 : 28;
-    default:
-      CONTRACT_ASSERT(false);
+namespace {
+  bool es_bisiesto(int a) {
+    return ((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0);
   }
-}
+
+  int dias_mes(int m, int a) {
+    CONTRACT_PRE(m > 0);
+    CONTRACT_PRE(m <= 12)
+    auto post = CONTRACT_POST_RESULT(r, r >= 28 && r <= 31);
+
+    std::vector<int> dias {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (m == 2) {
+      return post(es_bisiesto(a) ? 29 : 28);
+    } else {
+      return post(dias[m - 1]);// indices comienzan en cero
+    }
+  }
+}// namespace
 
 void suma_dia(fecha & f, int d) {
   while (d > 0) {
@@ -47,8 +49,7 @@ void suma_dia(fecha & f, int d) {
         f.mes = 1;
         ++f.anyo;
       }
-    }
-    else {
+    } else {
       f.dia += d;
       d = 0;
     }
